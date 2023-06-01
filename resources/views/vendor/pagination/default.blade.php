df8ad6a32dd69b7072626c33b1134a8e9155b6ee
42694 <congen@nkust.edu.tw>
2023-06-01 10:57:19 +0800

    Update

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -3,9 +3,9 @@
 
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a class="pagination-previous button is-link is-outlined" title="This is the first page" disabled><i class="fas fa-angle-left"></i> &nbsp; 上一頁</a>
+            <a class="pagination-previous button is-primary is-outlined is-rounded is-medium" title="This is the first page" disabled><i class="fas fa-angle-left"></i> </a>
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')" class="pagination-previous button is-link is-outlined"><i class="fas fa-angle-left"></i> &nbsp; 上一頁</a>
+            <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')" class="pagination-previous button is-primary is-outlined is-rounded is-medium"><i class="fas fa-angle-left"></i></a>
 
         @endif
         <ul class="pagination-list">
@@ -20,9 +20,9 @@
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li><a class="pagination-link is-current" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
+                        <li><a class="pagination-link button is-current is-rounded is-medium" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
                     @else
-                        <li><a  href="{{ $url }}" class="pagination-link button is-link is-outlined" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
+                        <li><a  href="{{ $url }}" class="pagination-link button is-primary is-outlined is-rounded is-medium" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
                     @endif
                 @endforeach
             @endif
@@ -30,9 +30,9 @@
         </ul>
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')" class="pagination-next button is-link is-outlined"><i class="fas fa-chevron-right"></i> &nbsp; 下一頁</a>
+            <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')" class="pagination-next button is-primary is-outlined is-rounded is-medium"><i class="fas fa-chevron-right"></i></a>
         @else
-        <a class="pagination-next button is-link is-link is-outlined" disabled>下一頁&nbsp;<i class="fas fa-chevron-right"></i> </a>
+        <a class="pagination-next button is-primary is-outlined is-rounded is-medium" disabled><i class="fas fa-chevron-right"></i> </a>
         @endif
 
     </nav>
--- resources/views/vendor/pagination/pagSearch.blade.php
+++ resources/views/vendor/pagination/pagSearch.blade.php
@@ -9,9 +9,9 @@
 
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a class="pagination-previous button is-link is-outlined" title="This is the first page" disabled><i class="fas fa-angle-left"></i> &nbsp; 上一頁</a>
+            <a class="pagination-previous button is-primary is-outlined is-rounded is-medium" title="This is the first page" disabled><i class="fas fa-angle-left"></i></a>
         @else
-            <a onclick="pageAction({{ isset($_GET['page']) ? $_GET['page']-1:1 }})" rel="prev" aria-label="@lang('pagination.previous')" class="pagination-previous button is-link is-outlined"><i class="fas fa-angle-left"></i> &nbsp; 上一頁</a>
+            <a onclick="pageAction({{ isset($_GET['page']) ? $_GET['page']-1:1 }})" rel="prev" aria-label="@lang('pagination.previous')" class="pagination-previous button is-primary is-outlined is-rounded is-medium"><i class="fas fa-angle-left"></i></a>
 
         @endif
         <ul class="pagination-list">
@@ -26,9 +26,9 @@
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li><a class="pagination-link is-current" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
+                        <li><a class="pagination-link button is-current is-rounded is-medium" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
                     @else
-                        <li><a onclick="pageAction({{ $page }})" class="pagination-link button is-link is-outlined" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
+                        <li><a onclick="pageAction({{ $page }})" class="pagination-link button is-primary is-outlined is-rounded is-medium" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
                     @endif
                 @endforeach
             @endif
@@ -36,9 +36,9 @@
         </ul>
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a onclick="pageAction({{ isset($_GET['page']) ? $_GET['page']+1:1 }})" rel="next" aria-label="@lang('pagination.next')" class="pagination-next button is-link is-outlined"><i class="fas fa-chevron-right"></i> &nbsp; 下一頁</a>
+            <a onclick="pageAction({{ isset($_GET['page']) ? $_GET['page']+1:1 }})" rel="next" aria-label="@lang('pagination.next')" class="pagination-next button is-primary is-outlined is-medium"><i class="fas fa-chevron-right"></i></a>
         @else
-        <a class="pagination-next button is-link is-outlined" disabled>下一頁&nbsp;<i class="fas fa-chevron-right"></i> </a>
+        <a class="pagination-next button is-primary is-outlined is-rounded is-medium" disabled><i class="fas fa-chevron-right"></i> </a>
         @endif
 
     </nav>
