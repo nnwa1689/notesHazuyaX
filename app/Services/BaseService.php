63a35c6f4994c19d52981b8e13b58d88e5292b32
42694 <congen@nkust.edu.tw>
2023-06-07 12:43:31 +0800

    ORM

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -27,7 +27,7 @@ class BaseService
     public function GetWebConfig()
     {
         DB::connection('mysql');
-        $data = Web::all() -> orderBy('ID', 'asc');
+        $data = Web::all() -> sortByDesc('ID');
         //$data = DB::select("SELECT * FROM web ORDER BY ID ASC");
         return $data;
     }
