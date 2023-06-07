25ae6de2174847df09974dee37811c2f26050c2b
42694 <congen@nkust.edu.tw>
2023-06-07 17:10:04 +0800

    ORM

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -25,7 +25,7 @@ class PostService
     {
         DB::connection('mysql');
         //$start = ($pageNumber - 1) * 10;
-        $data = Blog::where('Competence', 'on') -> orderBy('PostDate', 'desc') -> paginate(10);
+        $data = Blog::where('Competence', 'on') -> orderBy('PostDate', 'desc');
         //$data = DB::table(DB::raw("(SELECT Blog.*, BClasses.ClassName, admin.Yourname, admin.Avatar FROM Blog JOIN admin ON (Blog.UserID = admin.username) JOIN BClasses ON (Blog.ClassId = BClasses.ClassId) WHERE Blog.Competence='public') as Post ORDER BY Post.PostDate DESC"))->paginate(10);
         //$data = DB::select("SELECT * FROM Blog WHERE Blog.Competence=? OR Blog.Competence=? ORDER BY Blog.PostDate DESC LIMIT ?, 10", ['public', 'protect',$start]);
         return $data;
