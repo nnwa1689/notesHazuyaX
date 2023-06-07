29be35e525044743f0d56ba8bc02cdc6f7e832bc
42694 <congen@nkust.edu.tw>
2023-06-07 13:30:13 +0800

    ORM

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -2,8 +2,6 @@
 
 namespace App\Services;
 
-use Illuminate\Http\Request;
-use DB;
 use App\Services\UserService;
 use App\Services\PostService;
 use App\Services\NavbarService;
@@ -28,7 +26,6 @@ class BaseService
     {
         DB::connection('mysql');
         $data = Web::orderBy('ID', 'asc') -> get();
-        //$data = DB::select("SELECT * FROM web ORDER BY ID ASC");
         return $data;
     }
 
@@ -53,24 +50,25 @@ class BaseService
         DB::connection('mysql');
         try
         {
-            DB::update('update web set tittle = ? where ID = 0', [$req->webName]);
-            DB::update('update web set tittle = ? where ID = 13', [$req-> URL]);
-            DB::update('update web set tittle = ? where ID = 5', [$req->Logo]);
-            DB::update('update web set tittle = ? where ID = 1', [$req->keyWords]);
-            DB::update('update web set tittle = ? where ID = 2', [$req->descripition]);
-            DB::update('update web set tittle = ? where ID = 4', [$req->header]);
-            DB::update('update web set tittle = ? where ID = 3', [$req->footer]);
-            DB::update('update web set tittle = ? where ID = 7', [$req->HomePostNum]);
-            DB::update('update web set tittle = ? where ID = 20', [$req->FB]);
-            DB::update('update web set tittle = ? where ID = 21', [$req->IG]);
-            DB::update('update web set tittle = ? where ID = 22', [$req->TWITTER]);
-            DB::update('update web set tittle = ? where ID = 23', [$req->APPLEPODCAST]);
-            DB::update('update web set tittle = ? where ID = 24', [$req->GOOGLEPODCAST]);
+            Web::where('ID', 0) -> update(['tittle' => $req->webName]);
+            Web::where('ID', 1) -> update(['tittle' => $req->keyWords]);
+            Web::where('ID', 2) -> update(['tittle' => $req->descripition]);
+            Web::where('ID', 3) -> update(['tittle' => $req->footer]);
+            Web::where('ID', 4) -> update(['tittle' => $req->header]);
+            Web::where('ID', 5) -> update(['tittle' => $req->Logo]);
+            Web::where('ID', 7) -> update(['tittle' => $req->HomePostNum]);
+            Web::where('ID', 13) -> update(['tittle' => $req-> URL]);
+            Web::where('ID', 20) -> update(['tittle' => $req->FB]);
+            Web::where('ID', 21) -> update(['tittle' => $req->IG]);
+            Web::where('ID', 22) -> update(['tittle' => $req->TWITTER]);
+            Web::where('ID', 23) -> update(['tittle' => $req->APPLEPODCAST]);
+            Web::where('ID', 24) -> update(['tittle' => $req->GOOGLEPODCAST]);
+            Web::where('ID', 29) -> update(['tittle' => $req->quote]);
+
             //DB::update('update web set tittle = ? where ID = 25', [$req->HomeAds1Url]);
             //DB::update('update web set tittle = ? where ID = 26', [$req->Home1AdsImg]);
             //DB::update('update web set tittle = ? where ID = 27', [$req->Home2AdsUrl]);
             //DB::update('update web set tittle = ? where ID = 28', [$req->Home2AdsImg]);
-            DB::update('update web set tittle = ? where ID = 29', [$req->quote]);
         }
         catch(Exception $e)
         {
