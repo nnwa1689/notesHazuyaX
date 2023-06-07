3d88cf38623313adbc304bece75fce639a492ad3
Cong-En Wu <nnwa1689@gmail.com>
2023-06-07 23:56:39 +0800

    WORKS, WEB, USER, PAGE  TO ORM MODEL

--- resources/views/postindex.blade.php
+++ resources/views/postindex.blade.php
@@ -17,7 +17,8 @@
         </div>
     </div>
     <div class="columns is-multiline is-mobile is-gapless is-justify-content-center" style="margin-left: -1rem; margin-right: -1rem;">
-        @foreach($allPosts as $post)
+        @foreach($allPosts[0] as $post)
+            {{ $post }}
             @component('compoments.postitem',
                 ['url' => $webData['webConfig'][13]->tittle."post/".$post->PostId,
                 'CoverImage' => $post->CoverImage,
