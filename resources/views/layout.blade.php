6ce9b314d9cecc590d4153d7a79f10d583e40407
42694 <congen@nkust.edu.tw>
2023-06-14 15:32:26 +0800

    Change Page Test

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -263,5 +263,46 @@
             });
             
         </script>
+        <script>
+            function submit() {
+                var name = $("#Name").val();
+                var email = $("#Email").val();
+                var type = $("#Type").val();
+                var budgetranges = $("#BudgetRanges").val();
+                var content = $("#Content").val();
+
+                $("#suc").css("display", "none");
+                $("#error").css("display", "none");
+
+                if (name.length < 1 || email.length < 1 || content.length < 5) {
+                    $("#error").css("display", "block");
+                } else {
+                    $.ajax({
+                        type: "POST",
+                        url: "https://strapi.studio-44s.tw/api/holas",
+                        dataType: "json",
+                        data: {
+                            "data": {
+                                "Name": name,
+                                "Email": email,
+                                "Content": content,
+                                "BudgetRanges": budgetranges,
+                                "Statu": "unsettled",
+                                "Type": type
+                            }
+                        },
+                        success: function (response) {
+                            $("#suc").css("display", "block");
+                            $("#Name").val("");
+                            $("#Email").val("");
+                            $("#Content").val("");
+                        },
+                        error: function (thrownError) {
+                            $("#error").css("display", "block");
+                        }
+                    });
+                }
+            }
+        </script>
     </body>
 </html>
--- resources/views/vendor/pagination/pagSearch.blade.php
+++ resources/views/vendor/pagination/pagSearch.blade.php
@@ -1,45 +1,45 @@
-
-    <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+<nav class="pagination is-centered" role="navigation" aria-label="pagination">
     <script>
         function pageAction(page){
-            document.getElementById("page").setAttribute('value',page)
+            document.getElementById("page").setAttribute('value',page);
             document.getElementById("searchForm").submit();
+            var searchUrlStr = "{{$webData['webConfig'][13]->tittle}}"
+            return "{{$webData['webConfig'][13]->tittle}}"
         }
     </script>
 
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <a class="pagination-previous button is-primary is-rounded is-medium" title="This is the first page" disabled><i class="fas fa-angle-left"></i></a>
-        @else
-            <a onclick="pageAction({{ isset($_GET['page']) ? $_GET['page']-1:1 }})" rel="prev" aria-label="@lang('pagination.previous')" class="pagination-previous button is-primary is-rounded is-medium"><i class="fas fa-angle-left"></i></a>
+    {{-- Previous Page Link --}}
+    @if ($paginator->onFirstPage())
+        <a class="pagination-previous button is-primary is-rounded is-medium" title="This is the first page" disabled><i class="fas fa-angle-left"></i></a>
+    @else
+        <a href="{{$webData['webConfig'][13]->tittle}}/search/q?page={{ isset($_GET['page']) ? $_GET['page']-1:1 }}&search-text=$_GET['search-text']&_token=$_GET['_token']" rel="prev" aria-label="@lang('pagination.previous')" class="pagination-previous button is-primary is-rounded is-medium"><i class="fas fa-angle-left"></i></a>
 
-        @endif
-        <ul class="pagination-list">
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <li><span class="pagination-ellipsis">&hellip;</span></li>
-            @endif
-
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li><a class="pagination-link button is-current is-rounded is-medium" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
-                    @else
-                        <li><a onclick="pageAction({{ $page }})" class="pagination-link button is-primary is-rounded is-medium" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
-        </ul>
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <a onclick="pageAction({{ isset($_GET['page']) ? $_GET['page']+1:1 }})" rel="next" aria-label="@lang('pagination.next')" class="pagination-next button is-primary is-rounded is-medium"><i class="fas fa-chevron-right"></i></a>
-        @else
-        <a class="pagination-next button is-primary is-rounded is-medium" disabled><i class="fas fa-chevron-right"></i> </a>
+    @endif
+    <ul class="pagination-list">
+    {{-- Pagination Elements --}}
+    @foreach ($elements as $element)
+        {{-- "Three Dots" Separator --}}
+        @if (is_string($element))
+            <li><span class="pagination-ellipsis">&hellip;</span></li>
         @endif
 
-    </nav>
+        {{-- Array Of Links --}}
+        @if (is_array($element))
+            @foreach ($element as $page => $url)
+                @if ($page == $paginator->currentPage())
+                    <li><a class="pagination-link button is-current is-rounded is-medium" aria-label="Page {{ $page }}" aria-current="page">{{ $page }}</a></li>
+                @else
+                    <li><a href="{{$webData['webConfig'][13]->tittle}}/search/q?page={{ $page }}&search-text=$_GET['search-text']&_token=$_GET['_token']" class="pagination-link button is-primary is-rounded is-medium" aria-label="Goto page {{ $page }}">{{ $page }}</a></li>
+                @endif
+            @endforeach
+        @endif
+    @endforeach
+    </ul>
+    {{-- Next Page Link --}}
+    @if ($paginator->hasMorePages())
+        <a href="{{$webData['webConfig'][13]->tittle}}/search/q?page={{ isset($_GET['page']) ? $_GET['page']+1:1 }}&search-text=$_GET['search-text']&_token=$_GET['_token']" rel="next" aria-label="@lang('pagination.next')" class="pagination-next button is-primary is-rounded is-medium"><i class="fas fa-chevron-right"></i></a>
+    @else
+    <a class="pagination-next button is-primary is-rounded is-medium" disabled><i class="fas fa-chevron-right"></i> </a>
+    @endif
 
+</nav>
