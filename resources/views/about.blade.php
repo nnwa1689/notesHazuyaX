c8cced6111b7b054fd9a3aa3e57a1a59ef9ee8be
Cong-En Wu <nnwa1689@gmail.com>
2023-07-02 02:27:33 +0800

    自我滿足的修正-about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,15 +8,15 @@
         </p>
     </div>
     <br/>
-    <p class="is-size-4 mt-6" data-scroll>
-        <span class="has-text-link"><i class="fas fa-quote-left"></i></span><br/>
-        在資訊樣貌多元的今天，相信您有很多想和大家說的，也許是一段故事、一首歌、一道菜或是一個理念，我們也是！
-        因此我們的使命就是讓您所訴說的內容更易於被受眾看見並且理解，在他們心中種下一顆小小的火種。
-        <br/>
-        <span class="has-text-link"><i class="fas fa-quote-right"></i></span>
-    </p>
-
-    <section class="hero is-link is-halfheight mt-6" data-scroll data-scroll-speed="2">
+    <div class="mt-6">
+        <p class="has-text-link is-size-1 mb-6"><i class="fas fa-quote-left"></i></p>
+        <p class="is-size-3 has-text-centered"> 在資訊樣貌多元的今天，相信您有很多想和大家說的，</p>
+        <p class="is-size-3 has-text-centered"> 也許是一段故事、一首歌、一道菜或是一個理念，我們也是！</p>
+        <p class="is-size-3 has-text-centered">因此，我們的使命就是讓您所訴說的內容更易於被受眾看見並且理解，</p>
+        <p class="is-size-3 has-text-centered">在他們心中種下一顆小小的火種。</p>
+        <p class="has-text-link is-size-1 has-text-right mt-6"><i class="fas fa-quote-right"></i></p>
+    </div>
+    <section class="hero is-link is-halfheight" data-scroll data-scroll-speed="2">
         <div class="hero-body">
             <div class="container is-fluid has-text-left">
                 <p class="subtitle is-1">服務項目</p>
