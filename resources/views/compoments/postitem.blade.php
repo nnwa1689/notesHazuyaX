3e93aa24041358c045e39739332cce8eec0de1bf
42694 <congen@nkust.edu.tw>
2023-07-06 16:10:25 +0800

    文字修正

--- resources/views/compoments/postitem.blade.php
+++ resources/views/compoments/postitem.blade.php
@@ -1,24 +1,20 @@
-<div>
-    <div class="row">
-        <div class="column">
-            <div data-scroll data-scroll-speed="2" data-scroll-delay="1.5" onclick="barba.go('{{ $url }}')" class="is-post-item">
-                @if(isset($CoverImage) && !empty($CoverImage))
-                <img alt="{{$PostTittle}}" class="image" src="{{$CoverImage}}">
-                @else
-                <img alt="{{$PostTittle}}" class="image" src="/images/NotesHZ_ICON_2023.png">
-                @endif
-                <a class="button post-image-tag-category is-primary is-outlined is-rounded is-small" href="{{$CategoryUrl}}">
-                    <span>{{$Category}}</span>
-                </a>
-                <br/>
-                <a class="button post-image-tag is-primary is-outlined is-rounded is-medium">
-                    <span>{{ strip_tags(Str::limit($PostTittle, 30)) }}</span>
-                </a>
-            </div>
-            <p class="title is-6">
-                <span>{{ strip_tags(Str::limit($PostTittle, 30)) }}</span>
-            </p>
-        </div>
+<div class="is-post-item">
+    <div data-scroll data-scroll-speed="2" data-scroll-delay="1.5" onclick="barba.go('{{ $url }}')" class="is-post-item">
+        @if(isset($CoverImage) && !empty($CoverImage))
+        <img alt="{{$PostTittle}}" class="image" src="{{$CoverImage}}">
+        @else
+        <img alt="{{$PostTittle}}" class="image" src="/images/NotesHZ_ICON_2023.png">
+        @endif
+        <a class="button post-image-tag-category is-primary is-outlined is-rounded is-small" href="{{$CategoryUrl}}">
+            <span>{{$Category}}</span>
+        </a>
+        <br/>
+        <a class="button post-image-tag is-primary is-outlined is-rounded is-medium">
+            <span>{{ strip_tags(Str::limit($PostTittle, 30)) }}</span>
+        </a>
     </div>
+    <p class="title is-6">
+        <span>{{ strip_tags(Str::limit($PostTittle, 30)) }}</span>
+    </p>
 </div>
 
