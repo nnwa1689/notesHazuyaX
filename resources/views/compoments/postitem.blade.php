ea316cccc4ae5c1c3370d466b180d0fc63a97c38
42694 <congen@nkust.edu.tw>
2023-07-06 16:37:39 +0800

    文字修正

--- resources/views/compoments/postitem.blade.php
+++ resources/views/compoments/postitem.blade.php
@@ -8,11 +8,8 @@
         <a class="button post-image-tag-category is-primary is-outlined is-rounded is-small" href="{{$CategoryUrl}}">
             <span>{{$Category}}</span>
         </a>
-        <br/>
-        <a class="button post-image-tag is-primary is-outlined is-rounded is-medium">
-            <span>{{ strip_tags(Str::limit($PostTittle, 30)) }}</span>
-        </a>
     </div>
-    <a class="is-size-5 mt-5">{{ strip_tags(Str::limit($PostTittle, 30)) }}</p>
+    <br/>
+    <a class="is-size-4 has-text-weight-bold mt-5">{{ strip_tags(Str::limit($PostTittle, 30)) }}</p>
 </div>
 
