1a6031db5b61ce2e9cd6ae4c0d53fd4f051ff670
42694 <congen@nkust.edu.tw>
2023-07-07 10:40:22 +0800

    Works 樣式

--- resources/views/compoments/WorksItem.blade.php
+++ resources/views/compoments/WorksItem.blade.php
@@ -1,5 +1,5 @@
-<div class="is-WorksItem {{ ($i == 2) || $i == 3 ? 'is-works-item-min' : 'is-works-item-large' }}">
-    <div data-scroll data-scroll-speed="{{($i == 2) || $i == 3 ? '3' : '5'}}" data-scroll-delay="1.5" onclick="barba.go('{{$url}}')">
+<div class="is-WorksItem {{ ($i == 2) || $i == 3 ? 'is-works-item-min' : 'is-works-item-large' }}" data-scroll data-scroll-speed="{{($i == 2) || $i == 3 ? '3' : '5'}}" data-scroll-delay="1.5" onclick="barba.go('{{$url}}')">
+    <div class="is-WorksItem-img">
         <img class="image" style="max-width:1000px; max-height:1000px;" src="{{$CoverImage}}">
     </div>
     <a class="button works-image-tag is-primary is-medium">
