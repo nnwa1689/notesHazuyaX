2c81e00e78134532dd0c5564e9e1fb0be5b197af
Cong-En Wu <nnwa1689@gmail.com>
2023-07-08 23:47:13 +0800

    Mobile

--- resources/views/compoments/WorksItem.blade.php
+++ resources/views/compoments/WorksItem.blade.php
@@ -1,4 +1,4 @@
-<div class="is-WorksItem {{ ($i == 2) || $i == 3 ? 'is-works-item-min' : 'is-works-item-large' }} {{ $i % 2 == 0 ? 'ml-4' : 'mr-4' }}" data-scroll data-scroll-speed="3" data-scroll-delay="1.5" onclick="barba.go('{{$url}}')">
+<div class="is-WorksItem {{ ($i == 2) || $i == 3 ? 'is-works-item-min' : 'is-works-item-large' }} {{ $i % 2 == 0 ? 'ml-4' : 'mr-4' }}" data-scroll-speed="{{($i == 2) || $i == 3 ? '3' : '5'}}" data-scroll-delay="1.5" onclick="barba.go('{{$url}}')">
     <div class="is-WorksItem-img">
         <img class="image" src="{{$CoverImage}}">
     </div>
