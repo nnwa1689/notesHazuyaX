f9b89bed5464ef4a0219e5257cb6854e93d25580
Cong-En Wu <nnwa1689@gmail.com>
2023-07-09 01:04:55 +0800

    Works Images

--- resources/views/compoments/WorksItem.blade.php
+++ resources/views/compoments/WorksItem.blade.php
@@ -1,6 +1,6 @@
 <div data-scroll data-scroll-speed="{{($i == 2) || $i == 3 ? '3' : '5'}}" data-scroll-delay="1.5" class="is-WorksItem {{ ($i == 2) || $i == 3 ? 'is-works-item-min' : 'is-works-item-large' }} {{ $i % 2 == 0 ? 'ml-4' : 'mr-4' }}" onclick="barba.go('{{$url}}')">
     <div class="is-WorksItem-img">
-        <img alt="{{$WorksName}}" src="{{$CoverImage}}">
+        <img alt="{{$WorksName}}" class="image" src="{{$CoverImage}}">
     </div>
     <a class="button works-image-tag is-outlined is-medium">
         <span>{{$WorksName}}</span>
