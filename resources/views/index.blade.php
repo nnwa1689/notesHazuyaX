317c79749f748e47f1142d42feb0667bcc527658
WU-CONG-EN <wucongen@WU-CONG-ENdeMac-mini.local>
2023-08-05 01:18:52 +0800

    首頁日期特效

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,7 +22,7 @@
                 <div class="column has-text-right has-text-centered-mobile">
                     <p id="hour" class="title is-home has-text-primary has-text-shadow">14</p>
                     <p class="title is-2">
-                        <span class="has-text-link has-text-shadow"><i class="fas fa-circle mr-6"></i></span>
+                        <span class="has-text-link has-text-shadow"><i class="fas fa-circle mr-3"></i></span>
                         <span class="has-text-link has-text-shadow"><i class="fas fa-circle ml-4"></i></span>
                     </p>
                     <p id="min" class="title is-home has-text-primary has-text-shadow">44</p>
