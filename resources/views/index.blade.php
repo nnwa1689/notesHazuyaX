30140cdab323e1da79acde28d724fdf769028f1c
42694 <congen@nkust.edu.tw>
2023-09-13 11:10:32 +0800

    HOME TITLE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('title', $title)
 @section('content')
-<div data-scroll data-scroll-speed="-5" data-scroll-delay="1" class="container mb-6">
+<div data-scroll data-scroll-speed="-5" data-scroll-delay="0" class="container mb-6">
     <section class="hero is-small">
         <div class="hero-body">
             <div class="columns">
@@ -19,7 +19,7 @@
                 <div class="column has-text-centered">
                     <p id="hour" class="title is-1 has-text-primary has-text-shadow">14</p>
                     <p id="dot" class="title is-3 has-text-link has-text-shadow">
-                        <i class="fas fa-circle mb-3"></i>
+                        <i class="fas fa-circle mb-5"></i>
                         <br/>
                         <i class="fas fa-circle"></i>
                     </p>
@@ -30,7 +30,7 @@
         </div>
     </section>
 </div>
-<div data-scroll data-scroll-speed="2" data-scroll-delay="1" class="mt-3">
+<div data-scroll data-scroll-speed="2" data-scroll-delay="0" class="mt-3">
     <section class="hero is-link is-shadow is-halfheight">
         <div class="hero-body">
             <div class="">
@@ -75,7 +75,7 @@
         </div>
     </section>
 </div>
-<div data-scroll data-scroll-speed="-1" data-scroll-delay="0" class="mt-3">
+<div data-scroll data-scroll-speed="5" data-scroll-delay="0" class="mt-3">
     <section class="hero is-success is-shadow is-halfheight">
         <div class="hero-body">
             <div class="container is-fluid has-text-right">
