418159adc7297d8b81cdbfd4440c73d337406b55
42694 <congen@nkust.edu.tw>
2023-09-13 15:27:39 +0800

    HOME

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,50 +22,48 @@
                 <i class="fas fa-circle"></i>
             </p>
             <p id="min" class="title is-1 has-text-primary has-text-shadow">44</p>
-            <p class="subtitle is-4 has-text-primary mt-5">
-                <i class="fas fa-desktop mr-3"></i> 網站設計、前後端系統開發
-            </p>
         </div>
     </div>
+    <p class="subtitle is-4 has-text-primary mt-5 has-text-right has-text-centered-mobile">
+        <i class="fas fa-desktop mr-3"></i> 網站設計、前後端系統開發
+    </p>
 </div>
 <div data-scroll data-scroll-speed="1" class="mt-6 mb-3">
     <div class="container">
         <p class="title is-2">
             <span class="has-text-background-primary">近期作品</span>
         </p>
-        <p>在每次的作品中呈現一種令人放鬆的體驗，<br/>溫和的顏色、簡單的拼貼，再加上一點動感，讓平凡的資訊加上一些點綴。</p>
+        <p class="has-text-right has-text-left-mobile">在網路上找回單純的互動方式。</p>
     </div>
-    <section class="hero is-halfheight">
-        <div class="container p-3">
-            <div class="columns is-multiline is-mobile is-justify-content-center" style="align-items: end;">
-                @php($i = 1)
-                @foreach($worksData as $item)
-                    @component('compoments.WorksItem',
-                        ['url' => $webData['webConfig'][13]->tittle."works/".$item -> WorksID,
-                        'CoverImage' => $item -> CoverImage,
-                        'WorksName' => $item -> WorksName,
-                        'i' => $i,
-                        ])
-                    @endcomponent
-                @if($i == 0)
-                @php($i++)
-                @elseif($i == 1)
-                @php($i++)
-                @elseif($i == 2)
-                @php($i++)
-                @elseif($i == 3)
-                @php($i = $i - 3)
-                @endif
-                @endforeach
-            </div>
-            <p class="has-text-right">
-                <button class="button is-primary mt-6 mb-6 mr-3" onclick="barba.go('{{$webData['webConfig'][13]->tittle}}works')">
-                    更多作品
-                    <i class="fas fa-arrow-right ml-1"></i>
-                </button>
-            </p>
+    <div class="container p-3">
+        <div class="columns is-multiline is-mobile is-justify-content-center" style="align-items: end;">
+            @php($i = 1)
+            @foreach($worksData as $item)
+                @component('compoments.WorksItem',
+                    ['url' => $webData['webConfig'][13]->tittle."works/".$item -> WorksID,
+                    'CoverImage' => $item -> CoverImage,
+                    'WorksName' => $item -> WorksName,
+                    'i' => $i,
+                    ])
+                @endcomponent
+            @if($i == 0)
+            @php($i++)
+            @elseif($i == 1)
+            @php($i++)
+            @elseif($i == 2)
+            @php($i++)
+            @elseif($i == 3)
+            @php($i = $i - 3)
+            @endif
+            @endforeach
         </div>
-    </section>
+        <p class="has-text-right">
+            <button class="button is-primary is-large mt-6 mb-6 mr-3" onclick="barba.go('{{$webData['webConfig'][13]->tittle}}works')">
+                更多作品
+                <i class="fas fa-arrow-right ml-1"></i>
+            </button>
+        </p>
+    </div>
 </div>
 
 <div data-scroll data-scroll-speed="1" class="mb-3">
@@ -73,35 +71,32 @@
         <p class="title is-2">
             <span class="has-text-background-link">技術雜記</span>
         </p>
-        <p>製作祕辛、技術分享</p> 
+        <p class="has-text-right has-text-left-mobile">看看技術及瑣碎的日常。</p> 
     </div>
-
-    <section class="hero is-halfheight">
-        <div class="container p-3">
-            <div class="columns is-multiline">
-                @foreach($allPosts as $post)
-                    @component('compoments.postitem',
-                        ['url' => $webData['webConfig'][13]->tittle."blog/".$post->PostId,
-                        'CoverImage' => $post->CoverImage,
-                        'PostTittle' => $post->PostTittle,
-                        'PostContant' => $post->PostContant,
-                        'Category' => $post->Category->ClassName,
-                        'CategoryUrl' => $webData['webConfig'][13]->tittle."category/".$post->ClassId,
-                        'PostDate' => $post->PostDate,
-                        'ReadTime' => $post->ReadTime,
-                        'Author' => $post->Author->Yourname,
-                        'AuthorAvatarUrl' => $webData['webConfig'][13]->tittle.$post->Author->Avatar,
-                        'AuthorUrl' => ''
-                        ])
-                    @endcomponent
-                @endforeach
-            </div>
-            <button class="button is-link mt-3 mb-3 is-large" onclick="barba.go('{{$webData['webConfig'][13]->tittle}}blog')">
-                更多雜記
-                <i class="fas fa-arrow-right ml-1"></i>
-            </button>
+    <div class="container p-3">
+        <div class="columns is-multiline">
+            @foreach($allPosts as $post)
+                @component('compoments.postitem',
+                    ['url' => $webData['webConfig'][13]->tittle."blog/".$post->PostId,
+                    'CoverImage' => $post->CoverImage,
+                    'PostTittle' => $post->PostTittle,
+                    'PostContant' => $post->PostContant,
+                    'Category' => $post->Category->ClassName,
+                    'CategoryUrl' => $webData['webConfig'][13]->tittle."category/".$post->ClassId,
+                    'PostDate' => $post->PostDate,
+                    'ReadTime' => $post->ReadTime,
+                    'Author' => $post->Author->Yourname,
+                    'AuthorAvatarUrl' => $webData['webConfig'][13]->tittle.$post->Author->Avatar,
+                    'AuthorUrl' => ''
+                    ])
+                @endcomponent
+            @endforeach
         </div>
-    </section>
+        <button class="button is-link mt-3 mb-3 is-large" onclick="barba.go('{{$webData['webConfig'][13]->tittle}}blog')">
+            更多雜記
+            <i class="fas fa-arrow-right ml-1"></i>
+        </button>
+    </div>
 </div>
 
 <button data-scroll class="button is-fullwidth is-large is-link mt-6 p-6" style="min-height: 100px; border-radius: 15px;" onclick="barba.go('{{$webData['webConfig'][13]->tittle}}contact')">
