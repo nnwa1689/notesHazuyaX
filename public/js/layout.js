9e28c9ebe4af55d047d6b4090c8e137db2fbaeec
42694 <congen@nkust.edu.tw>
2023-09-15 09:12:54 +0800

    menu

--- public/js/layout.js
+++ public/js/layout.js
@@ -4,7 +4,7 @@
 
 const openMenu = () => {
     gsap.to(".fullMenu", {
-            duration: 0.6,
+            duration: 0.5,
             width: "100%",
             ease: "power3.inOut"
         });
@@ -12,7 +12,7 @@ const openMenu = () => {
 
 const closeMenu = () => {
     gsap.to(".fullMenu", {
-            duration: 0.6,
+            duration: 0.5,
             width: "0%",
             ease: "power3.inOut"
         });
