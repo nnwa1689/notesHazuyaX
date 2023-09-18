ef77e357d2faf37a3cdd45c8fe19c4ddb3352be1
42694 <congen@nkust.edu.tw>
2023-09-18 17:22:41 +0800

    Works Item

--- resources/views/worksList.blade.php
+++ resources/views/worksList.blade.php
@@ -2,7 +2,7 @@
 @section('title', $title)
 @section('content')
 <div class="container">
-    <div data-scroll data-scroll-speed="-1" data-scroll-delay="0" class="mb-6">
+    <div data-scroll class="mb-6">
         <p class="title is-1 has-text-left"><span id="titleText"></span></p>
     </div>
     <div class="columns is-multiline is-gapless is-mobile is-justify-left" style="align-items: end;">
