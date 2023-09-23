500c7adc99ce73062537907793db5fb21e9462eb
42694 <congen@nkust.edu.tw>
2023-09-23 13:09:00 +0800

    Custom Cursor

--- resources/views/compoments/WorksItem.blade.php
+++ resources/views/compoments/WorksItem.blade.php
@@ -1,8 +1,8 @@
-<a data-scroll class="is-WorksItem is-works-item-min mt-2" onclick="barba.go('{{$url}}')">
+<div data-scroll class="is-WorksItem is-works-item-min mt-2" onclick="barba.go('{{$url}}')">
     <div class="is-WorksItem-img">
         <img alt="{{$WorksName}}" src="{{$CoverImage}}">
     </div>
     <a class="button works-image-tag is-outlined is-medium">
         <span>{{$WorksName}}</span>
     </a>
-</a>
+</div>
